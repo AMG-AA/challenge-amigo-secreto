a74bb40a8c820b1e3daf7205175be73d7ef765db
AMG-AA <aggarcia1504@gmail.com>
2025-02-13 17:55:07 -0600

    Adicion de funciones para ejecutar el programa

--- app.js
+++ app.js
@@ -3,15 +3,41 @@
 let friends = []
 
 
-function addFriend(name){
-    name = document.getElementById('amigo').value;
+// Add a friend to the list 
+function agregarAmigo(){
+    const name = document.getElementById('amigo').value.trim();
+    // Check if the input is empty
     if (name === "") {
-        alert("Please enter a name. / Por favor ingrese un nombre.");
+        alert("Please, enter a name. / Por favor, inserte un nombre.");
+        return;
+    } else { 
+        friends.push(name);
+        document.getElementById('amigo').value = "";
+        showFriends();
+    }
+}
+
+// Show the friends list
+function showFriends(){
+    let list = document.getElementById('listaAmigos').innerHTML = "";
+    for (let i = 0; i < friends.length; i++){
+        list += `<li>${friends[i]}</li>`;
+    }
+    document.getElementById('listaAmigos').innerHTML = list;
+}
+
+
+// Sort a friend in the list
+function sortearAmigo(){
+    // Check if the array is empty
+    if (friends.length === 0) {
+        alert("Please enter a name before sorting. / Por favor ingrese un nombre antes de sortear.");
         return;
     } else {
-        friends.push(name);
-        console.log(friends);
-        document.getElementById('amigo').value = "";
-        document.getElementById('amigo').focus();
+        // Sort the array in aleatory order
+        let index = Math.floor(Math.random() * friends.length);
+        const friend = friends[index];
+        document.getElementById('resultado').innerHTML = `El amigo seleccionado es ${friend}`;
     }
+    
 }
